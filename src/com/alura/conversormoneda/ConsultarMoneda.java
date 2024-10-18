4c177e2e00c25d9fcc4c8763ff05527a26505168
Yineth Morales <yinethmaritzamoralesberrio@gmail.com>
2024-10-18 13:39:54 -0500

    Modificacion final clase consultar moneda

--- src/com/alura/conversormoneda/ConsultarMoneda.java
+++ src/com/alura/conversormoneda/ConsultarMoneda.java
@@ -1,4 +1,30 @@
 package com.alura.conversormoneda;
 
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsultarMoneda {
+    public Divisas buscarMoneda(String monedaBase, String monedaTarget){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/1bea9ba82b34ab82e0dee981/pair/"
+                + monedaBase + "/" + monedaTarget);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Divisas.class);
+        }
+        catch (Exception e){
+            throw new RuntimeException("No se encontro la divisa");
+        }
+    }
+
 }
